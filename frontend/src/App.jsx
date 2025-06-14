dfc2c09e94eeace66f25a8612d0cb4bf38172232
Manuely Meireles Lemos <manumeireleslemos@gmail.com>
2025-06-14 18:49:45 -0300

    Adding vite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
+import { useState } from 'react'
+import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Usuarios from "./pages/Usuarios";
+import Usuarios from "../pages/Usuarios";
+import Animais from "../pages/Animais";
 
 export default function App() {
   return (
     <Router>
       <nav>
         <Link to="/usuarios">Usuários</Link>
-        {/* <Link to="/animais">Animais</Link> */}
+        <Link to="/animais">Pets</Link>
       </nav>
       <Routes>
         <Route path="/usuarios" element={<Usuarios />} />
-        {/* <Route path="/animais" element={<Animais />} /> */}
+        <Route path="/animais" element={<Animais />} /> 
       </Routes>
     </Router>
   );
-}
+}S
